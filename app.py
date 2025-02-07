9f669299acca3f063fa70cae07d1efe2b80ef6cf
Leonidas MV <yordy.lmv.2000@gmail.com>
2025-02-07 11:00:12 +0100

    add success.html

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Render asigna un puerto automáticamente
-    app.run(host='0.0.0.0', port=port)
-    # app.run(debug=True)
+    # port = int(os.environ.get("PORT", 10000))  # Render asigna un puerto automáticamente
+    # app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
