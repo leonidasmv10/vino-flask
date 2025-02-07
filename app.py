c3a7e2f17a625c2735bab90f40cb080acbccd42b
Leonidas MV <yordy.lmv.2000@gmail.com>
2025-02-07 16:37:31 +0100

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
 from dotenv import load_dotenv
 
@@ -21,14 +21,22 @@ def services():
     return render_template("services.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        email = request.form["email"]
+        mensaje = request.form["mensaje"]
+        return redirect(url_for("success", nombre=nombre, email=email, mensaje=mensaje))
     return render_template("contact.html")
 
 
 @app.route("/success")
 def success():
-    return render_template("success.html")
+    nombre = request.args.get("nombre")
+    email = request.args.get("email")
+    mensaje = request.args.get("mensaje")
+    return render_template("success.html", nombre=nombre, email=email, mensaje=mensaje)
 
 
 if __name__ == "__main__":
