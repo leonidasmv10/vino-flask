646eebc7de885649bb86c0ada5c8b0808b9b1cba
Leonidas MV <yordy.lmv.2000@gmail.com>
2025-02-07 11:15:14 +0100

    fix

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def contact():
     return render_template('contact.html')
 
 @app.route('/success')
-def contact():
+def success():
     return render_template('success.html')
 
 if __name__ == '__main__':
